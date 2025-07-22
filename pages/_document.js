6fd508da17d1c6db1c1af5f28dac215dab0fa3fa
anny12sstr <anastasia.shpilka06@gmail.com>
2025-07-22 15:24:51 +0300

    Some improvments for logo

--- pages/_document.js
+++ pages/_document.js
@@ -38,10 +38,10 @@ export default function Document() {
         <link rel="alternate" hrefLang="zh" href="https://tableconverter.com/zh/" /> 
         <link rel="alternate" hrefLang="x-default" href="https://tableconverter.com/en/" />
 
-<link rel="icon" type="image/x-icon" href="/favicon.ico" />
-  <link rel="shortcut icon" href="/favicon.ico" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Source+Sans+Pro:wght@600&family=Cinzel:wght@600&family=Fira+Sans:wght@600&display=swap"
